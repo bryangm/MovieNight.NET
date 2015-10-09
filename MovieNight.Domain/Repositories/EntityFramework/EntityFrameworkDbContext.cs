d8cc68b0c3db1a4ec1c1ee2d7c7fcad76775ad3c
Bryan Mack <bmack@ons.org>
2015-10-09 14:14:05 -0400

    moving from MongoDB to EF

---
+++ MovieNight.Domain/Repositories/EntityFramework/EntityFrameworkDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MovieNight.Domain.Entities;
+
+namespace MovieNight.Domain.Repositories.EntityFramework
+{
+    public class EntityFrameworkDbContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Night> Nights { get; set; }
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Submission> Submissions { get; set; }
+    }
+}
